0f1dc49d7ed5451c390a44bf6b60e03ff185dfa6
Aðalsteinn Jónsson <adalsteinn.jonsson@pearlgroup.is>
2025-04-14 11:30:16 +0000

    Clean up

--- JsonSqlConfigDb/Extension/JsonSqlExtension.cs
+++ JsonSqlConfigDb/Extension/JsonSqlExtension.cs
@@ -9,18 +9,6 @@ namespace JsonSqlConfigDb.Extension
 {
     public static class JsonSqlExtension
     {
-        public static IServiceCollection AddJsonSqlConfigDb(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsAction, bool scopedService = true)
-        {
-            services.AddDbContext<JsonSqlContext>(optionsAction);
-            if (scopedService) services.AddScoped<IJsonSqlService, JsonSqlService>();
-            else services.AddTransient<IJsonSqlService, JsonSqlService>();
-
-            // Store options action
-            JsonSqlContext.OptionsAction = optionsAction;
-
-            return services;
-        }
-
         public static IServiceCollection AddJsonSqlConfigDb(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddOptions<JsonSqlSettings>()
